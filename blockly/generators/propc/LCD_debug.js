66a59b302a4032e0321006aa91f08388b7b91424
Vale <valetolpegin@gmail.com>
2014-11-06 10:46:23 -0500

    Update LCD_debug.js

--- blockly/generators/propc/LCD_debug.js
+++ blockly/generators/propc/LCD_debug.js
@@ -34,19 +34,19 @@ Blockly.Language.debug_lcd_init = {
     category: 'Debug LCD',
     helpUrl: '',
     init: function() {
-        this.setColour(180);
-        this.appendDummyInput("")
-                .appendTitle("LCD init")
-                .appendTitle("PIN#")
-                .appendTitle(new Blockly.FieldDropdown(profile.default.digital), "PIN");
-        this.appendDummyInput("")
-                .appendTitle("Baud")
-                .appendTitle(new Blockly.FieldDropdown([["2400", "2400"], ["9600", "9600"], ["19200", "19200"]]), "BAUD");
-        this.appendDummyInput("")
-                .appendTitle("Lines")
-                .appendTitle(new Blockly.FieldDropdown([["2", "2"], ["4", "4"]]), "LINES");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
+        this.setColour( 180 );
+        this.appendDummyInput( "" )
+                .appendTitle( "LCD init" )
+                .appendTitle( "PIN#" )
+                .appendTitle( new Blockly.FieldDropdown( profile.default.digital ), "PIN" );
+        this.appendDummyInput( "" )
+                .appendTitle( "Baud" )
+                .appendTitle( new Blockly.FieldDropdown( [["2400", "2400"], ["9600", "9600"], ["19200", "19200"]] ), "BAUD" );
+        this.appendDummyInput( "" )
+                .appendTitle( "Lines" )
+                .appendTitle( new Blockly.FieldDropdown( [["2", "2"], ["4", "4"]] ), "LINES" );
+        this.setPreviousStatement( true, null );
+        this.setNextStatement( true, null );
     }
 };
 
@@ -54,11 +54,11 @@ Blockly.Language.debug_lcd_clear = {
     category: 'Debug LCD',
     helpUrl: '',
     init: function() {
-        this.setColour(180);
-        this.appendDummyInput("")
-                .appendTitle("LCD clear");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
+        this.setColour( 180 );
+        this.appendDummyInput( "" )
+                .appendTitle( "LCD clear" );
+        this.setPreviousStatement( true, null );
+        this.setNextStatement( true, null );
     }
 };
 
@@ -66,16 +66,16 @@ Blockly.Language.debug_lcd_print = {
     category: 'Debug LCD',
     helpUrl: '',
     init: function() {
-        this.setColour(180);
-        this.appendDummyInput("")
-                .appendTitle("LCD print")
-                .appendTitle(new Blockly.FieldImage(Blockly.pathToBlockly +
-                        'media/quote0.png', 12, 12))
-                .appendTitle(new Blockly.FieldTextInput(''), 'TEXT')
-                .appendTitle(new Blockly.FieldImage(Blockly.pathToBlockly +
-                        'media/quote1.png', 12, 12));
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
+        this.setColour( 180 );
+        this.appendDummyInput( "" )
+                .appendTitle( "LCD print" )
+                .appendTitle( new Blockly.FieldImage( Blockly.pathToBlockly +
+                        'media/quote0.png', 12, 12 ) )
+                .appendTitle( new Blockly.FieldTextInput( '' ), 'TEXT' )
+                .appendTitle( new Blockly.FieldImage( Blockly.pathToBlockly +
+                        'media/quote1.png', 12, 12 ) );
+        this.setPreviousStatement( true, null );
+        this.setNextStatement( true, null );
     }
 };
 
@@ -83,13 +83,14 @@ Blockly.Language.debug_lcd_number = {
     category: 'Debug LCD',
     helpUrl: '',
     init: function() {
-        this.setColour(180);
-        this.appendDummyInput("")
-                .appendTitle("LCD print");
-        this.appendDummyInput("").appendTitle("Number");
-        this.appendValueInput('NUMBER').setCheck(Number);
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
+        this.setColour( 180 );
+        this.appendDummyInput( "" )
+                .appendTitle( "LCD print" );
+        this.appendDummyInput( "" ).appendTitle( "Number" );
+        this.appendValueInput( 'NUMBER' )
+            .setCheck( Number );
+        this.setPreviousStatement( true, null );
+        this.setNextStatement( true, null );
     }
 };
 
@@ -97,68 +98,73 @@ Blockly.Language.debug_lcd_action = {
     category: 'Debug LCD',
     helpUrl: '',
     init: function() {
-        this.setColour(180);
-        this.appendDummyInput("")
-                .appendTitle("LCD action")
-                .appendTitle(new Blockly.FieldDropdown([["Newline", "13"]]), "ACTION");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
+        this.setColour( 180 );
+        this.appendDummyInput( "" )
+                .appendTitle( "LCD action" )
+                .appendTitle( new Blockly.FieldDropdown( [["Newline", "13"]] ), "ACTION" );
+        this.setPreviousStatement( true, null );
+        this.setNextStatement( true, null );
     }
 };
 
 // define generators
-Blockly.Spin = Blockly.Generator.get('Spin');
+Blockly.propc = Blockly.Generator.get( 'propc' );
 
-Blockly.Spin.debug_lcd_init = function() {
+Blockly.propc.debug_lcd_init = function() {
     var dropdown_pin = this.getTitleValue('PIN');
     var baud = this.getTitleValue('BAUD');
     var lines = this.getTitleValue('LINES');
 
-    Blockly.Spin.definitions_['define_debug_lcd'] = 'OBJDEBUGLCD : "debug_lcd"';
-    Blockly.Spin.setups_['setup_debug_lcd'] = 'DEBUGLCD.init(' + dropdown_pin + ', ' + baud + ', ' + lines + ')';
+    //TO DO: HOW TO INITIALIZE AN LCD
+    Blockly.propc.definitions_['define_debug_lcd'] = '';
+    Blockly.propc.setups_['setup_debug_lcd'] = '';
 
     return '';
 };
 
-Blockly.Spin.debug_lcd_clear = function() {
+Blockly.propc.debug_lcd_clear = function() {
 
-    Blockly.Spin.definitions_['define_debug_lcd'] = 'OBJDEBUGLCD : "debug_lcd"';
-    if (Blockly.Spin.setups_['setup_debug_lcd'] == undefined) {
-        Blockly.Spin.setups_['setup_debug_lcd'] = 'DEBUGLCD.init(0, 9600, 2)';
+    //TO DO: HOW TO CLEAR AN LCD
+    Blockly.propc.definitions_['define_debug_lcd'] = '';
+    if (Blockly.propc.setups_['setup_debug_lcd'] === undefined) {
+        Blockly.propc.setups_['setup_debug_lcd'] = '';
     }
 
-    return 'DEBUGLCD.cls\n';
+    return '\n';
 };
 
-Blockly.Spin.debug_lcd_print = function() {
+Blockly.propc.debug_lcd_print = function() {
     var text = this.getTitleValue('TEXT');
 
-    Blockly.Spin.definitions_['define_debug_lcd'] = 'OBJDEBUGLCD : "debug_lcd"';
-    if (Blockly.Spin.setups_['setup_debug_lcd'] == undefined) {
-        Blockly.Spin.setups_['setup_debug_lcd'] = 'DEBUGLCD.init(0, 9600, 2)';
+    //TO DO: HOW TO PRINT TO AN LCD
+    Blockly.propc.definitions_['define_debug_lcd'] = '';
+    if (Blockly.propc.setups_['setup_debug_lcd'] == undefined) {
+        Blockly.propc.setups_['setup_debug_lcd'] = '';
     }
 
-    return 'DEBUGLCD.str(string("' + text + '"))\n';
+    return '\n';
 };
 
-Blockly.Spin.debug_lcd_number = function() {
-    var number = Blockly.Spin.valueToCode(this, 'NUMBER', Blockly.Spin.ORDER_UNARY_PREFIX) || '0';
+Blockly.propc.debug_lcd_number = function() {
+    var number = Blockly.propc.valueToCode(this, 'NUMBER', Blockly.propc.ORDER_UNARY_PREFIX) || '0';
 
-    Blockly.Spin.definitions_['define_debug_lcd'] = 'OBJDEBUGLCD : "debug_lcd"';
-    if (Blockly.Spin.setups_['setup_debug_lcd'] == undefined) {
-        Blockly.Spin.setups_['setup_debug_lcd'] = 'DEBUGLCD.init(0, 9600, 2)';
+    //TO DO: ADD C CODE
+    Blockly.propc.definitions_['define_debug_lcd'] = '';
+    if (Blockly.propc.setups_['setup_debug_lcd'] == undefined) {
+        Blockly.propc.setups_['setup_debug_lcd'] = '';
     }
 
-    return 'DEBUGLCD.dec(' + number + ')\n';
+    return '\n';
 };
 
-Blockly.Spin.debug_lcd_action = function() {
+Blockly.propc.debug_lcd_action = function() {
     var action = this.getTitleValue('ACTION');
 
-    Blockly.Spin.definitions_['define_debug_lcd'] = 'OBJDEBUGLCD : "debug_lcd"';
-    if (Blockly.Spin.setups_['setup_debug_lcd'] == undefined) {
-        Blockly.Spin.setups_['setup_debug_lcd'] = 'DEBUGLCD.init(0, 9600, 2)';
+    //TO DO: ADD C CODE
+    Blockly.propc.definitions_['define_debug_lcd'] = '';
+    if (Blockly.propc.setups_['setup_debug_lcd'] == undefined) {
+        Blockly.propc.setups_['setup_debug_lcd'] = '';
     }
 
-    return 'DEBUGLCD.putc(' + action + ')\n';
+    return '\n';
 };
